a0ce7504f0b5f3f21d80d43650dd473127169dc4
unknown <coocoojames@gmail.com>
2022-03-16 15:04:30 -0500

    Mapping linestrings done

--- Mapping_GeoJSON_Linestrings/js/logic.js
+++ Mapping_GeoJSON_Linestrings/js/logic.js
@@ -1,6 +1,6 @@
 console.log('Working!');
 
-let cityData = cities;
+// let cityData = cities;
 
 // let map = L.map('mapID').setView([30, 30], 2);
 
@@ -8,7 +8,7 @@ let cityData = cities;
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox/streets-v11',
+    id: 'mapbox/light-v10',
     tileSize: 512,
     zoomOffset: -1,
     accessToken: api_key
@@ -31,31 +31,31 @@ let baseMaps = {
 };
 
 let map = L.map(mapID, {
-    center: [30, 30],
+    center: [44.0, -80.0],
     zoom: 2,
     layers: [streets]
 });
 
 L.control.layers(baseMaps).addTo(map);
 
-let SFAirport =
-{"type":"FeatureCollection","features":[{
-    "type":"Feature",
-    "properties":{
-        "id":"3469",
-        "name":"San Francisco International Airport",
-        "city":"San Francisco",
-        "country":"United States",
-        "faa":"SFO",
-        "icao":"KSFO",
-        "alt":"13",
-        "tz-offset":"-8",
-        "dst":"A",
-        "tz":"America/Los_Angeles"},
-        "geometry":{
-            "type":"Point",
-            "coordinates":[-122.375,37.61899948120190]}}
-]};
+// let SFAirport =
+// {"type":"FeatureCollection","features":[{
+//     "type":"Feature",
+//     "properties":{
+//         "id":"3469",
+//         "name":"San Francisco International Airport",
+//         "city":"San Francisco",
+//         "country":"United States",
+//         "faa":"SFO",
+//         "icao":"KSFO",
+//         "alt":"13",
+//         "tz-offset":"-8",
+//         "dst":"A",
+//         "tz":"America/Los_Angeles"},
+//         "geometry":{
+//             "type":"Point",
+//             "coordinates":[-122.375,37.61899948120190]}}
+// ]};
 
 // // PointToLayer function
 // L.geoJSON(SFAirport, {
@@ -75,14 +75,20 @@ let SFAirport =
 //         layer.bindPopup('<h2> Airport Code: ' + feature.properties.faa + '</h2>');
 //     }
 // }).addTo(map);
-
-let airportData = 'https://raw.githubusercontent.com/coocoojames/Mapping_Earthquakes/main/majorAirports.json';
-d3.json(airportData).then((data) => {
+let weirdStyle = {
+    color: '#ffffa1',
+    weight: 2
+}
+let torontoData = 'https://raw.githubusercontent.com/coocoojames/Mapping_Earthquakes/GeoJSON_Linestrings/Mapping_GeoJSON_Linestrings/torontoRoutes.json';
+d3.json(torontoData).then((data) => {
     console.log(data);
     L.geoJSON(data, {
-        pointToLayer: (feature, latlng) => {
-            console.log(latlng);
-            return L.marker(latlng).bindPopup('<h1> Airport Code: ' + feature.properties.faa + '</h1>')
+        // color: '#ffffa1',
+        // weight: 2,
+        style: weirdStyle,
+        onEachFeature: (feature, layer) => {
+            console.log(layer);
+            layer.bindPopup('<h1> Airlines: ' + feature.properties.airline + '</h1>')
         }
     }).addTo(map);
 })
