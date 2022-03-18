f51a06549c7ff29c4ea24938dc64d1ff31afbc7d
unknown <coocoojames@gmail.com>
2022-03-18 11:46:58 -0500

    Mapping earthquakes with layers, done

--- Earthquakes_7days/js/logicStep1.js
+++ Earthquakes_7days/js/logicStep1.js
@@ -16,27 +16,33 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{
 
 // streets.addTo(map);
 
-let dark = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let sat = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox/dark-v10',
+    id: 'mapbox/satellite-streets-v11',
     tileSize: 512,
     zoomOffset: -1,
     accessToken: api_key
 });
 
 let baseMaps = {
-    Street: streets,
-    Dark: dark
+    'Street View': streets,
+    'Satellite View': sat
 };
 
 let map = L.map(mapID, {
     center: [39.5, -98.5],
-    zoom: 2,
+    zoom: 4,
     layers: [streets]
 });
 
-L.control.layers(baseMaps).addTo(map);
+let earthquakes = new L.layerGroup();
+
+let overlays = {
+    Earthquakes: earthquakes
+}
+
+L.control.layers(baseMaps, overlays).addTo(map);
 
 // let SFAirport =
 // {"type":"FeatureCollection","features":[{
@@ -75,22 +81,83 @@ L.control.layers(baseMaps).addTo(map);
 //         layer.bindPopup('<h2> Airport Code: ' + feature.properties.faa + '</h2>');
 //     }
 // }).addTo(map);
-let weirdStyle = {
-    color: '#ffffa1',
-    weight: 2
+
+function getColor(magnitude) {
+    if (magnitude > 5) {
+        return '#ea2c2c';
+    }
+    else if (magnitude > 4) {
+        return '#ea822c';
+    }
+    else if (magnitude > 3) {
+        return '#ee9c00';
+    }
+    else if (magnitude > 2) {
+        return '#eecc00';
+    }
+    else if (magnitude > 1) {
+        return '#d4ee00';
+    }
+    return '#98ee00';
 }
-let torontoData = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson';
-d3.json(torontoData).then((data) => {
+
+function getMag(magnitude) {
+    if (magnitude === 0) {
+        return 1;
+    }
+    return magnitude * 4
+}
+
+function weirdStyle(blah) {
+    return {
+        color: '#ffffa1',
+        opacity: 1,
+        fillOpacity: 1,
+        fillColor: getColor(blah.properties.mag),
+        radius: getMag(blah.properties.mag),
+        stroke: true,
+        weight: 0.5
+}};
+
+let legend = L.control({
+    position: 'bottomright'
+});
+
+legend.onAdd = function() {
+    let div = L.DomUtil.create('div', 'info legend');
+
+    const magnitudes = [0, 1, 2, 3, 4, 5];
+    const colors = ["#98ee00", "#d4ee00", "#eecc00", "#ee9c00", "#ea822c", "#ea2c2c"];
+
+    for (var i = 0; i < magnitudes.length; i++) {
+        div.innerHTML +=
+        "<i style='background: " + colors[i] + "'></i> " + 
+        magnitudes[i] + (magnitudes[i+1] ? '&ndash;' + magnitudes[i+1] + '<br>' : '+');
+    };
+    return div;
+};
+legend.addTo(map);
+
+let quakes = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson';
+d3.json(quakes).then((data) => {
     console.log(data);
     L.geoJSON(data, {
         // color: '#ffffa1',
         // weight: 2,
         style: weirdStyle,
+        pointToLayer: (feature, latlng) => {
+            console.log(latlng)
+            return L.circleMarker(latlng)
+        },
         onEachFeature: (feature, layer) => {
             console.log(layer);
-            layer.bindPopup('<h1> Airlines: ' + feature.properties.airline + '</h1>')
+            layer.bindPopup('<h2> Magnitude: ' + feature.properties.mag + '</h2> <hr> <h3>' 
+            + feature.properties.place + '</h3>')
         }
-    }).addTo(map);
+    }).addTo(earthquakes);
+    earthquakes.addTo(map);
 })
+
+
 
 console.log('Still workin baby!');
